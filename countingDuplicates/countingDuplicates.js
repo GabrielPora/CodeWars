daaaee60cd717fa99a07e26d07d2b5e295c68a1a
GabrielPora <gabriel.groener@gmail.com>
2018-03-14 18:54:59 +0200

    passed, needs refactor

--- countingDuplicates/countingDuplicates.js
+++ countingDuplicates/countingDuplicates.js
@@ -1,33 +1,32 @@
 function duplicateCount(text){
-	//...
 	var tmp = "";
+	var tmptext = text.toLowerCase();
 	var count = 0;
 	var max = 0;
-  //   console.log("text.length: ", text.length);
+	console.log("text.length: ", text.length);
 	
-	for(i = 0; i < text.length; i++)
+	
+	for(i = 0; i < tmptext.length; i++)
 	{
-	  tmp = text[i].toLowerCase();
-  //     console.log("tmp: ",tmp);
-	  for(j = i+1; j < text.length; j++)
+   console.log("tmptext: ", tmptext);
+		tmp = tmptext[i];
+      console.log("check tmp: ",tmp);
+	  for(j = 0; j < tmptext.length; j++)
 	  {      
-  //       console.log("text: ",text[j].toLowerCase());
-		if (tmp == text[j].toLowerCase())
-		  if(count == 0)
-			count +=2;
-		  else
-			count++;
+        console.log("tmp: ",tmptext[j]);
+    		if (tmp == tmptext[j] && tmp != " ")
+    			count++;           
 	  }    
-  //     console.log("count: ",count);
-	  if (max < count)
-		max = count;
+      console.log("count: ",count);
+      var re = new RegExp(tmp, "g");
+      tmptext = tmptext.replace(re, " ");
+	  if (count > 1)
+		  max++;
 	  count = 0;
-  //     console.log("loop: ", i)
-  //     console.log("max: ",max);
-  //     console.log("\n");
-	}  
-	  max = max % 5;    
-  //     console.log("Final max: ",max);
+      console.log("loop: ", i)
+      console.log("max: ",max);
+      console.log("\n");
+	}    
+      console.log("Final max: ",max);
 	  return max;
-  //     return -1;
   }
