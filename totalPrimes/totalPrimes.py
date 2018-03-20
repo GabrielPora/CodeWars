9dbab1bfef7fce79a4d6d2777335c41e48b3d3cf
GabrielPora <gabriel.groener@gmail.com>
2018-03-20 18:19:50 +0200

    updated

--- totalPrimes/totalPrimes.py
+++ totalPrimes/totalPrimes.py
@@ -1,5 +1,22 @@
 from math import sqrt
 from itertools import count, islice
+import gmpy2
+# import sympy
+
+def primes(a, b):
+    prime = []
+    n = a
+    while n < b:
+        n = gmpy2.next_prime(n)
+        prime.append(n)
+        n+=1;
+#     while True:
+#         yield b
+#         n = gmpy2.next_prime(n)
+#         prime.append(n)
+#         print ("prime: ", prime)
+    print ("prime: ", prime)
+    return prime
 
 def isPrime(n):
     if n < 2: return False
@@ -11,15 +28,17 @@ def isPrime(n):
 
 def get_total_primes(a, b):
     # Happy coding!
-    prime = []
+#     primes(a, b)
+    prime = primes(a, b)
     count = 0
     countCheck = 0
     tmpindexing = 0
     index = a
-    while index < b:
-        if isPrime(index):
-            prime.append(index)
-        index+=1        
+#     prime = list(sympy.primerange(a, b))
+#     while index < b:
+#         if isPrime(index):
+#             prime.append(index)
+#         index+=1        
     
     for tmpindex in prime:
         number_string = str(tmpindex)
@@ -27,6 +46,8 @@ def get_total_primes(a, b):
         for ch in number_string:
             if isPrime(int(ch)):
                 countCheck+=1
+            else:
+                break
         if indexingCount == countCheck:
             count+=1
         countCheck = 0
