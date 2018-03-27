20a0e2cb6b98ef0f31ef3f811994cb2ad6c678c5
GabrielPora <gabriel.groener@gmail.com>
2018-03-27 07:29:40 +0200

    finally added better test and I think it will pass now

--- Demo_OfferZen/demo.js
+++ Demo_OfferZen/demo.js
@@ -3,28 +3,94 @@
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
-    A = A.sort(function(a, b){return a- b});
-    var small = 0 ;
-    var pre = 0;
-    var next = 0;
-    var diff = 0;
-    for(var i = A[0]; i < A[A.length-1]; i++)
+    A = A.sort(function(a, b) {return a- b});
+    var small = 0;
+	var pre = A[0];
+	var next = A[1];
+	var check = A[2];
+  var diff = 0;
+  var len = A.length;
+//   len = A[len-1];
+//   var len = A[A.length] -1;
+       console.log("A: ", A);
+       console.log("length: ", len)
+    if (next == 0 &&  pre == 0 && check == 0)
     {
-        pre = A[i];
-        next = A[i+1];
-        diff = next - pre;
-        if(pre > 0 && next > 0 && diff > 1)
+        A = A.sort(function(a, b) {return b-a});
+       console.log("runnning here : ", A);
+       var tmp =[];
+            pre = A[0];
+            next = A[1];
+            diff = pre - next;
+//         if (!diff)
+//         {
+//           console.log("YESRDFSDF");
+//         }
+        for(var i = 0; i < len; i++)
         {
+            pre = A[i];
+            next = A[i+1];
+            diff = pre - next;
+            console.log("diff: ", diff);
+            if(pre > 0 && next == 0 && diff > 1)
+            { 
+                small = pre - 1;
+                console.log("fgdfnhvhv  con it run: ", diff);
+                return small;
+            }
+            if(pre > 0 && next >= 0)
+               tmp[i] = A[i];
+        }
+        tmp = tmp.sort(function(a, b) {return a- b});
+        console.log("Tmp Array: ", tmp);
+        len = tmp.length;
+        for(var i = 0; i < len; i++) // Start Row
+    	{
+          pre = tmp[i];
+          next = tmp[i+1];
+           diff = next - pre;
+           console.log("i: ", i);
+           console.log("pre: ", pre);
+           console.log("next: ", next);
+           console.log("diff: ", diff);
+         if(pre >= 0 && next >= 0 && diff > 1)
+         { 
             small = pre + 1;
             return small;
+          }
+    	}
+        if (small == 0)
+        {
+            var stringtmp = String(tmp[tmp.length-1]);
+            small = parseInt(stringtmp) + 1;
+            if (small <= 0)
+                small = 1;
         }
     }
-    if (small == 0)
+    else
     {
-        var stringtmp = String(A[A.length - 1]);
-        small = parseInt(stringtmp) + 1;
-        if (small <= 0)
-            small = 1;
+        for(var i = 0; i < len; i++) // Start Row
+    	{
+          pre = A[i];
+          next = A[i+1];
+           diff = next - pre;
+           console.log("i: ", i);
+           console.log("pre: ", pre);
+           console.log("next: ", next);
+           console.log("diff: ", diff);
+         if(pre >= 0 && next >= 0 && diff > 1)
+         { 
+            small = pre + 1;
+            return small;
+          }
+    	}
+        if (small == 0)
+        {
+            var stringtmp = String(A[A.length-1]);
+            small = parseInt(stringtmp) + 1;
+            if (small <= 0)
+                small = 1;
+        }
     }
     return small;
 }
---
+++ Demo_OfferZen/test.js
@@ -0,0 +1,19 @@
+Test.assertEquals(solution([1, 3, 6, 4, 1, 2]), 5);
+Test.assertEquals(solution([0]), 1);
+Test.assertEquals(solution([1]), 2);
+Test.assertEquals(solution([-1]), 1);
+Test.assertEquals(solution([1, 2, 3]), 4);
+Test.assertEquals(solution([-1, -3]), 1);
+Test.assertEquals(solution([1, 2, 2, 2, 3, 4]), 5);
+Test.assertEquals(solution([-1, 2, 2, 2, -3, -8]), 3);
+Test.assertEquals(solution([-1, 2, -2, 2, -3, -8, -12, -2]), 3);
+Test.assertEquals(solution([-1, -2, -2, -2, -3, -8, -12, -2]), 1);
+Test.assertEquals(solution([0, -2, -2, -2, -3, 8, -12, -5]), 1); 
+Test.assertEquals(solution([1, 2, 2, 2, 3, 8 , 0 , 0, 0 , -1, -2, -3, 0]), 4);
+Test.assertEquals(solution([0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 9]), 1);
+Test.assertEquals(solution([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]), 2);
+Test.assertEquals(solution([0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 9]), 3);
+Test.assertEquals(solution([1, 2, 2, 2, 3, 8 , 0 , 0, 0 ]), 4);
+// Test.assertEquals(duplicateCount("aabBcde"), 2,"should ignore case");
+// Test.assertEquals(duplicateCount("Indivisibility"), 1)
+// Test.assertEquals(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent")
